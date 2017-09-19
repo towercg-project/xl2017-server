0ac8210d55566586cc959cbcda1ae2bb2fe6d32a
Ed Ropple <ed@edropple.com>
2017-09-19 03:47:37 -0400

    wip

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const TowerCG = require('@towercg/server');
 const ExamplePlugin = require('@towercg-plugins/example-server').ExamplePlugin;
+const TwitchPlugin = require('@towercg-plugins/twitch-server').TwitchPlugin;
+const TimersPlugin = require('@towercg-plugins/timers-server').TimersPlugin;
+const secrets = require('./secrets.json');
 
 const cfg = new TowerCG.Config(__dirname);
-cfg.registerPlugin(ExamplePlugin);
+
+cfg.logging.logAllEvents = false;
+
+cfg.registerPlugin(ExamplePlugin, {
+  tickRate: 60000,
+  logTick: false
+});
+cfg.registerPlugin(TwitchPlugin, {
+  identity: secrets.twitch,
+  apiDebug: false,
+  chatDebug: false
+});
+cfg.registerPlugin(TimersPlugin);
 
 TowerCG.start(cfg);
