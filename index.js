fbae3540d6eca45e2e04bd576fb10f600469448c
Ed Ropple <ed@edropple.com>
2017-11-02 16:12:16 -0400

    added extra life plugin, disabled twitch for now

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TowerCG = require('@towercg/server');
 const ExamplePlugin = require('@towercg-plugins/example-server').ExamplePlugin;
+const ExtraLifePlugin = require('@towercg-plugins/extralife-server').ExtraLifePlugin;
 const TwitchPlugin = require('@towercg-plugins/twitch-server').TwitchPlugin;
 const TimersPlugin = require('@towercg-plugins/timers-server').TimersPlugin;
 const secrets = require('./secrets.json');
@@ -12,11 +13,20 @@ cfg.registerPlugin(ExamplePlugin, {
   tickRate: 60000,
   logTick: false
 });
-cfg.registerPlugin(TwitchPlugin, {
-  identity: secrets.twitch,
-  apiDebug: false,
-  chatDebug: false
-});
+// cfg.registerPlugin(TwitchPlugin, {
+//   identity: secrets.twitch,
+//   apiDebug: false,
+//   chatDebug: false
+// });
 cfg.registerPlugin(TimersPlugin);
+cfg.registerPlugin(ExtraLifePlugin, {
+  users: {
+    'ed': '250675'
+  },
+  teams: {
+    'cic': '36773',
+    'giant-bomb': '33133'
+  }
+});
 
 TowerCG.start(cfg);
